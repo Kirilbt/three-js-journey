fd29436404399d3db158a58afccf39a7d17987f1
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-08 12:21:48 +0200

    Adding Contact Material

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -40,12 +40,27 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World()
 world.gravity.set(0, -9.82, 0)
 
+// Materials
+const concreteMaterial = new CANNON.Material('concrete')
+const plasticMaterial = new CANNON.Material('plastic')
+
+const concretePlasticContactMaterial = new CANNON.ContactMaterial(
+  concreteMaterial,
+  plasticMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.9
+  }
+)
+world.addContactMaterial(concretePlasticContactMaterial)
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5)
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
-  shape: sphereShape
+  shape: sphereShape,
+  material: plasticMaterial
 })
 world.addBody(sphereBody)
 
@@ -54,12 +69,12 @@ const floorShape = new CANNON.Plane()
 const floorBody = new CANNON.Body()
 floorBody.mass = 0
 floorBody.addShape(floorShape)
+floorBody.material = concreteMaterial
 floorBody.quaternion.setFromAxisAngle(
   new CANNON.Vec3(-1, 0, 0),
   Math.PI * 0.5
 )
 world.addBody(floorBody)
-
 
 /**
  * Test sphere
