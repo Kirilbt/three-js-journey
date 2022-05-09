0d2eea7c91ffeee76f64caa0f9f261ffb6266d60
Kiril Tucker <kiril.tucker@gmail.com>
2022-05-09 12:48:41 +0200

    Updated Cannon to Cannon-es

--- 22-physics/src/script.js
+++ 22-physics/src/script.js
@@ -2,7 +2,7 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'lil-gui'
-import CANNON from 'cannon'
+import * as CANNON from 'cannon-es'
 
 /**
  * Debug
