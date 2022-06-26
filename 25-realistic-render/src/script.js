696350fc94788ec18a212d61b861f20feea7f89a
Kiril Tucker <kiril.tucker@gmail.com>
2022-06-26 22:40:26 +0200

    25 - Realistic Render: Shadow Bias and Normal Bias

--- 25-realistic-render/src/script.js
+++ 25-realistic-render/src/script.js
@@ -61,11 +61,12 @@ gui.add(debugObject, 'envMapIntensity').min(0).max(10).step(0.001).onChange(upda
  * Models
  */
  gltfLoader.load(
-  '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+  // '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+  '/models/hamburger.glb',
   (gltf) => {
-    gltf.scene.scale.set(10, 10, 10)
-    gltf.scene.position.set(0, -4, 0)
-    gltf.scene.rotation.y = Math.PI * 0.5
+    gltf.scene.scale.set(0.3, 0.3, 0.3) // Helmet settings: 10, 10, 10
+    gltf.scene.position.set(0, -1, 0) // Helmet settings: 0, -4, 0
+    // gltf.scene.rotation.y = Math.PI * 0.5
     scene.add(gltf.scene)
 
     gui.add(gltf.scene.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('rotation')
@@ -82,6 +83,7 @@ directionalLight.position.set(0.25, 3, -2.25)
 directionalLight.castShadow = true
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.normalBias = 0.05
 scene.add(directionalLight)
 
 const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
