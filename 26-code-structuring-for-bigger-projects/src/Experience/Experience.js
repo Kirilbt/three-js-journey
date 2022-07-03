59ae3dbcbaf04bca6aff0d518b54f5bfa548a51e
Kiril Tucker <kiril.tucker@gmail.com>
2022-07-03 22:50:00 +0200

    26 - Code Structuring: Adding Utils

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,6 +1,36 @@
+import Sizes from "./Utils/Sizes.js"
+import Time from "./Utils/Time.js"
+
+
 export default class Experience {
-  constructor() {
+  constructor(canvas) {
     // Global Access
     window.experience = this
+
+    // Options
+    this.canvas = canvas
+    console.log(this.canvas)
+
+    // Setup
+    this.sizes = new Sizes()
+    this.time = new Time()
+
+    // Sizes Resize Event
+    this.sizes.on('resize', () => {
+      this.resize()
+    })
+
+    // Time Tick Event
+    this.time.on('tick', () => {
+      this.update()
+    })
+  }
+
+  resize() {
+
+  }
+
+  update() {
+
   }
 }
--- 26-code-structuring-for-bigger-projects/src/script.js
+++ 26-code-structuring-for-bigger-projects/src/script.js
@@ -1,7 +1,7 @@
 import './style.css'
 import Experience from './Experience/Experience.js'
 
-const experience = new Experience()
+const experience = new Experience(document.querySelector('canvas.webgl'))
 
 // import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
