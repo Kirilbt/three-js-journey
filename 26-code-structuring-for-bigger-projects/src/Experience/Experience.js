babb049238d99a660e15cfbb4593ab43bce752db
Kiril Tucker <kiril.tucker@gmail.com>
2022-07-04 11:34:22 +0200

    26 - Code Structuring: Renderer

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -2,6 +2,7 @@ import * as THREE from 'three'
 import Sizes from "./Utils/Sizes.js"
 import Time from "./Utils/Time.js"
 import Camera from './Camera.js'
+import Renderer from './Renderer.js'
 
 let instance = null
 
@@ -26,6 +27,7 @@ export default class Experience {
     this.time = new Time()
     this.scene = new THREE.Scene()
     this.camera = new Camera()
+    this.renderer = new Renderer()
 
     // Sizes Resize Event
     this.sizes.on('resize', () => {
@@ -40,9 +42,11 @@ export default class Experience {
 
   resize() {
     this.camera.resize()
+    this.renderer.resize()
   }
 
   update() {
     this.camera.update()
+    this.renderer.update()
   }
 }
