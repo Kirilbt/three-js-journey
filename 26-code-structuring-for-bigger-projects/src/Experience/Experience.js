1d95c5b215cb57c4ff4b6e26c5ec4cc68ab5a5b4
Kiril Tucker <kiril.tucker@gmail.com>
2022-07-04 11:21:54 +0200

    26 - Code Structuring: Camera (and singleton)

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -1,10 +1,19 @@
 import * as THREE from 'three'
 import Sizes from "./Utils/Sizes.js"
 import Time from "./Utils/Time.js"
+import Camera from './Camera.js'
+
+let instance = null
 
 
 export default class Experience {
   constructor(canvas) {
+    if(instance) {
+      return instance
+    }
+
+    instance = this
+
     // Global Access
     window.experience = this
 
@@ -16,6 +25,7 @@ export default class Experience {
     this.sizes = new Sizes()
     this.time = new Time()
     this.scene = new THREE.Scene()
+    this.camera = new Camera()
 
     // Sizes Resize Event
     this.sizes.on('resize', () => {
@@ -29,10 +39,10 @@ export default class Experience {
   }
 
   resize() {
-
+    this.camera.resize()
   }
 
   update() {
-
+    this.camera.update()
   }
 }
