87e744a5bdddbe32bf7e156c12dea5c87d6a9966
Kiril Tucker <kiril.tucker@gmail.com>
2022-07-06 20:19:12 +0200

    26 - Code Structuring: Destroy

--- 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/Experience.js
@@ -23,7 +23,6 @@ export default class Experience {
 
     // Options
     this.canvas = canvas
-    console.log(this.canvas)
 
     // Setup
     this.debug = new Debug()
@@ -55,5 +54,32 @@ export default class Experience {
     this.camera.update()
     this.world.update()
     this.renderer.update()
+  }
+
+  destroy() {
+    this.sizes.off('resize')
+    this.time.off('tick')
+
+    // Traverse the Whole Scene
+    this.scene.traverse((child) => {
+      if(child instanceof THREE.Mesh) {
+        child.geometry.dispose()
+
+        for(const key in child.material) {
+          const value = child.material[key]
+
+          if(value && typeof value.dispose === 'function') {
+            value.dispose()
+          }
+        }
+      }
+
+      this.camera.controls.dispose()
+      this.renderer.instance.dispose()
+
+      if(this.debug.active) {
+        this.debug.ui.destroy()
+      }
+    })
   }
 }
