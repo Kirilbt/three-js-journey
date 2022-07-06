a3a65515963fd52d6b68a4d22d449c102549e7ca
Kiril Tucker <kiril.tucker@gmail.com>
2022-07-06 17:38:31 +0200

    26 - Code Structuring: Environment Map

--- 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
+++ 26-code-structuring-for-bigger-projects/src/Experience/World/Environment.js
@@ -5,8 +5,10 @@ export default class Environment {
   constructor() {
     this.experience = new Experience()
     this.scene = this.experience.scene
+    this.resources = this.experience.resources
 
     this.setSunLight()
+    this.setEnvironmentMap()
   }
 
   setSunLight() {
@@ -19,4 +21,24 @@ export default class Environment {
     this.scene.add(this.sunLight)
   }
 
+  setEnvironmentMap() {
+    this.environmentMap = {}
+    this.environmentMap.intensity = 0.4
+    this.environmentMap.texture = this.resources.items.environmentMapTexture
+    this.environmentMap.texture.encoding = THREE.sRGBEncoding
+
+    this.scene.environment = this.environmentMap.texture
+
+    this.environmentMap.updateMaterials = () => {
+      this.scene.traverse((child) => {
+        if(child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+          child.material.envMap = this.environmentMap.texture
+          child.material.envMapIntensity = this.environmentMap.intensity
+          child.material.needsUpdate = true
+        }
+      })
+    }
+
+    this.environmentMap.updateMaterials()
+  }
 }
