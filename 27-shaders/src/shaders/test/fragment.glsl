5ad3548d354754cc5950d93007fdb88f9d8f0df0
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-03 19:36:45 +0200

    27 - Shaders: Varyings

--- 27-shaders/src/shaders/test/fragment.glsl
+++ 27-shaders/src/shaders/test/fragment.glsl
@@ -1,5 +1,7 @@
 precision mediump float;
 
+varying float vRandom;
+
 void main() {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(0.5, vRandom, 1.0, 1.0);
 }
--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -5,6 +5,8 @@ uniform mat4 modelMatrix;
 attribute vec3 position;
 attribute float aRandom;
 
+varying float vRandom;
+
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
   // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
@@ -14,5 +16,6 @@ void main() {
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
-  // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+
+  vRandom = aRandom;
 }
