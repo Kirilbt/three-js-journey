e34f3bbbae5684535ad961eb663b0afe3b7d72e1
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-03 19:27:52 +0200

    27 - Shaders: Attributes

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -3,10 +3,12 @@ uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
 attribute vec3 position;
+attribute float aRandom;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+  // modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+  modelPosition.z += aRandom * 0.1;
 
   vec4 viewPosition =  viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
