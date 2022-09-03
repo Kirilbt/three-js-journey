c562b4370580d6a0f6d976e680364a3ddd188f26
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-03 20:46:03 +0200

    27 - Shaders: Added Color Variations

--- 27-shaders/src/shaders/test/vertex.glsl
+++ 27-shaders/src/shaders/test/vertex.glsl
@@ -8,15 +8,20 @@ attribute vec3 position;
 attribute vec2 uv;
 
 varying vec2 vUv;
+varying float vElevation;
 
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-  modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-  modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+  elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
+  modelPosition.z += elevation;
 
   vec4 viewPosition =  viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
   vUv = uv;
+  vElevation = elevation;
 }
