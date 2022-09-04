2f76f8a6c5500a79b9a66b99b0acf1f1e4de85e1
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-04 14:29:54 +0200

    28 - Shader Patterns: Pattern 15

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -40,12 +40,21 @@ void main() {
   // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
   // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-  // Pattern 14
-  float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
-  barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+  // // Pattern 14
+  // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+  // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-  float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
-  barY *= step(0.8, mod(vUv.x * 10.0, 1.0));
+  // float barY = step(0.8, mod(vUv.x * 10.0, 1.0));
+  // barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
+
+  // float strength = barX + barY;
+
+  // Pattern 15
+  float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+  barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+
+  float barY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+  barY *= step(0.4, mod(vUv.y * 10.0, 1.0));
 
   float strength = barX + barY;
 
