0156a8d3a04ad827a0e4cfdeebc205138df56939
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-05 15:42:20 +0200

    28 - Shader Patterns: Pattern 42

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -191,9 +191,16 @@ void main() {
 
   // gl_FragColor = vec4(strength, strength, strength, 1.0);
 
-  // Pattern 41
+  // // Pattern 41
+  // float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+  // float strength = angle;
+
+  // Pattern 42
   float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+  angle /= PI * 2.0;
+  angle += 0.5;
   float strength = angle;
+
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
