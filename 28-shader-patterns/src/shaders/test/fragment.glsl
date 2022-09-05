bf6c90183130e848b96553f920281ce96e4fdacb
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-05 14:50:06 +0200

    28 - Shader Patterns: Pattern 29

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -110,9 +110,11 @@ void main() {
   // // float strength = length(vUv - 0.5);
   // float strength = distance(vUv, vec2(0.5, 0.5));
 
-  // Pattern 28
-  // float strength = length(vUv - 0.5);
-  float strength = 1.0 - distance(vUv, vec2(0.5, 0.5));
+  // // Pattern 28
+  // float strength = 1.0 - distance(vUv, vec2(0.5, 0.5));
+
+  // Pattern 29
+  float strength = 0.015 / distance(vUv, vec2(0.5, 0.5));
 
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);
