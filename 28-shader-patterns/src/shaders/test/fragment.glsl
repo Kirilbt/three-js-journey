ac22d9f2f87c1d468f4fdfa9cc2d93e849e517df
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-05 15:25:16 +0200

    28 - Shader Patterns: Pattern 40

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -178,12 +178,16 @@ void main() {
   // );
   // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
-  // Pattern 39
-  vec2 wavedUv = vec2(
-    vUv.x + sin(vUv.y * 100.0) * 0.1,
-    vUv.y + sin(vUv.x * 100.0) * 0.1
-  );
-  float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+  // // Pattern 39
+  // vec2 wavedUv = vec2(
+  //   vUv.x + sin(vUv.y * 100.0) * 0.1,
+  //   vUv.y + sin(vUv.x * 100.0) * 0.1
+  // );
+  // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+
+  // Pattern 40
+  float angle = atan(vUv.x, vUv.y);
+  float strength = angle;
 
   gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
