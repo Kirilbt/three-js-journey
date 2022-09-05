b943eb5a39967d40353ea10522feee94e9db9b0e
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-05 15:07:21 +0200

    28 - Shader Patterns: Pattern 32

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -1,7 +1,16 @@
+#define PI 3.14159265359
+
 varying vec2 vUv;
 
 float random(vec2 st) {
   return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid) {
+  return vec2(
+    cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+    cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+  );
 }
 
 void main() {
@@ -123,11 +132,22 @@ void main() {
   // );
   // float strength = 0.015 / distance(lightUv, vec2(0.5, 0.5));
 
-  // Pattern 31
-  vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+  // // Pattern 31
+  // vec2 lightUvX = vec2(vUv.x * 0.1 + 0.45, vUv.y * 0.5 + 0.25);
+  // float lightX = 0.015 / distance(lightUvX, vec2(0.5, 0.5));
+
+  // vec2 lightUvY = vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25);
+  // float lightY = 0.015 / distance(lightUvY, vec2(0.5, 0.5));
+
+  // float strength = lightX * lightY;
+
+  // Pattern 32
+  vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+
+  vec2 lightUvX = vec2(rotatedUv.x * 0.1 + 0.45, rotatedUv.y * 0.5 + 0.25);
   float lightX = 0.015 / distance(lightUvX, vec2(0.5, 0.5));
 
-  vec2 lightUvY = vec2(vUv.y * 0.1 + 0.45, vUv.x * 0.5 + 0.25);
+  vec2 lightUvY = vec2(rotatedUv.y * 0.1 + 0.45, rotatedUv.x * 0.5 + 0.25);
   float lightY = 0.015 / distance(lightUvY, vec2(0.5, 0.5));
 
   float strength = lightX * lightY;
