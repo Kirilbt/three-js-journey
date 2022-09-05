02cd552a03d24cb23ed8f23318ae4644308996f1
Kiril Tucker <kiril.tucker@gmail.com>
2022-09-05 15:22:14 +0200

    28 - Shader Patterns: Pattern 39

--- 28-shader-patterns/src/shaders/test/fragment.glsl
+++ 28-shader-patterns/src/shaders/test/fragment.glsl
@@ -171,10 +171,17 @@ void main() {
   // );
   // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
-  // Pattern 38
+  // // Pattern 38
+  // vec2 wavedUv = vec2(
+  //   vUv.x + sin(vUv.y * 30.0) * 0.1,
+  //   vUv.y + sin(vUv.x * 30.0) * 0.1
+  // );
+  // float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+
+  // Pattern 39
   vec2 wavedUv = vec2(
-    vUv.x + sin(vUv.y * 30.0) * 0.1,
-    vUv.y + sin(vUv.x * 30.0) * 0.1
+    vUv.x + sin(vUv.y * 100.0) * 0.1,
+    vUv.y + sin(vUv.x * 100.0) * 0.1
   );
   float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
 
