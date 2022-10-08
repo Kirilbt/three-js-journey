559fe50ee0634f7b8ad12890b10072d37e03ae9c
Kiril Tucker <kiril.tucker@gmail.com>
2022-10-08 21:46:55 +0200

    Fixing the Normals

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -96,12 +96,20 @@ material.onBeforeCompile = (shader) => {
     `
   )
   shader.vertexShader = shader.vertexShader.replace(
-    '#include <begin_vertex>',
+    '#include <beginnormal_vertex>',
     `
-      #include <begin_vertex>
+      #include <beginnormal_vertex>
 
       float angle = (position.y + uTime) * 0.9;
       mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+      objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+      #include <begin_vertex>
 
       transformed.xz = rotateMatrix * transformed.xz;
     `
