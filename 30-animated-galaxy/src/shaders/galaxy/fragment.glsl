a3723c5cd8c4e606b364ee12a81fcbaf70c73511
Kiril Tucker <kiril.tucker@gmail.com>
2022-10-08 17:45:34 +0200

    Handling Colors

--- 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/fragment.glsl
@@ -1,3 +1,5 @@
+varying vec3 vColor;
+
 void main() {
   // // Disc
   // float strength = distance(gl_PointCoord, vec2(0.5));
@@ -13,5 +15,8 @@ void main() {
   strength = 1.0 - strength;
   strength = pow(strength, 10.0);
 
-  gl_FragColor = vec4(vec3(strength), 1.0);
+  // Final Color
+  vec3 color = mix(vec3(0.0), vColor, strength);
+
+  gl_FragColor = vec4(color, 1.0);
 }
--- 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
+++ 30-animated-galaxy/src/shaders/galaxy/vertex.glsl
@@ -2,6 +2,8 @@ uniform float uSize;
 
 attribute float aScale;
 
+varying vec3 vColor;
+
 void main() {
   /**
     * Position
@@ -16,4 +18,9 @@ void main() {
     */
   gl_PointSize = uSize * aScale;
   gl_PointSize *= (1.0 / - viewPosition.z);
+
+  /*
+  * Color
+  */
+  vColor = color;
 }
