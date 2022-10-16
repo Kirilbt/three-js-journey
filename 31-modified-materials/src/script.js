33eac236b613eea2a07a1105e7620fd41a108743
Kiril Tucker <kiril.tucker@gmail.com>
2022-10-16 20:44:57 +0200

    Added Post-processing Passes

--- 31-modified-materials/src/script.js
+++ 31-modified-materials/src/script.js
@@ -3,6 +3,15 @@ import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import * as dat from 'lil-gui'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
+import { DotScreenPass } from 'three/examples/jsm/postprocessing/DotScreenPass.js'
+import { RGBShiftShader } from 'three/examples/jsm/shaders/RGBShiftShader.js'
+import { GlitchPass } from 'three/examples/jsm/postprocessing/GlitchPass.js'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js'
+import { GammaCorrectionShader} from 'three/examples/jsm/shaders/GammaCorrectionShader.js'
+import { SMAAPass } from 'three/examples/jsm/postprocessing/SMAAPass.js'
 
 /**
  * Base
@@ -197,6 +206,10 @@ window.addEventListener('resize', () =>
     // Update renderer
     renderer.setSize(sizes.width, sizes.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+    // Update Effect Composer
+    effectComposer.setSize(sizes.width, sizes.height)
+    effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -228,6 +241,77 @@ renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 /**
+ * Post-Processing
+ */
+// Render Target
+const renderTarget = new THREE.WebGLRenderTarget(
+  800,
+  600,
+  {
+    samples: renderer.getPixelRatio() === 1 ? 2 : 0
+  }
+)
+
+// Effect Composer
+const effectComposer = new EffectComposer(renderer, renderTarget)
+effectComposer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+effectComposer.setSize(sizes.width, sizes.height)
+
+// Render Pass
+const renderPass = new RenderPass(scene, camera)
+effectComposer.addPass(renderPass)
+
+// Dot Screen Pass
+const dotScreenPass = new DotScreenPass()
+dotScreenPass.enabled = false
+dotScreenPass.uniforms[ 'scale' ].value = 1;
+effectComposer.addPass(dotScreenPass)
+
+gui.add(dotScreenPass, 'enabled').name('dotScreenPass')
+gui.add(dotScreenPass.uniforms[ 'scale' ], 'value').min(0).max(1).step(0.001).name('dotScreenPass_scale')
+
+// Glitch Pass
+const glitchPass = new GlitchPass()
+glitchPass.goWild = false
+glitchPass.enabled = false
+effectComposer.addPass(glitchPass)
+
+gui.add(glitchPass, 'enabled').name('glitchPass')
+gui.add(glitchPass, 'goWild').name('glitchPass_goWild')
+
+// Unreal Bloom Pass
+const unrealBloomPass = new UnrealBloomPass()
+unrealBloomPass.enabled = false
+unrealBloomPass.strength = 0.3
+unrealBloomPass.radius = 1
+unrealBloomPass.threshold = 0.6
+effectComposer.addPass(unrealBloomPass)
+
+gui.add(unrealBloomPass, 'enabled').name('unrealBloomPass')
+gui.add(unrealBloomPass, 'strength').min(0).max(2).step(0.001).name('unrealBloomPass_strength')
+gui.add(unrealBloomPass, 'radius').min(0).max(2).step(0.001).name('unrealBloomPass_radius')
+gui.add(unrealBloomPass, 'threshold').min(0).max(1).step(0.001).name('unrealBloomPass_threshold')
+
+// RGB Shift Pass
+const rgbShiftPass = new ShaderPass(RGBShiftShader)
+rgbShiftPass.enabled = false
+effectComposer.addPass(rgbShiftPass)
+
+gui.add(rgbShiftPass, 'enabled').name('rgbShiftPass')
+
+// Gamma Correction Pass
+const gammaCorrectionPass = new ShaderPass(GammaCorrectionShader)
+effectComposer.addPass(gammaCorrectionPass)
+
+// Antialias Pass
+if(renderer.getPixelRatio() === 1 && !renderer.capabilities.isWebGL2) {
+  const smaaPass = new SMAAPass()
+  effectComposer.addPass(smaaPass)
+
+  console.log('using SMAA');
+}
+
+/**
  * Animate
  */
 const clock = new THREE.Clock()
@@ -243,7 +327,8 @@ const tick = () =>
     controls.update()
 
     // Render
-    renderer.render(scene, camera)
+    // renderer.render(scene, camera)
+    effectComposer.render()
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
