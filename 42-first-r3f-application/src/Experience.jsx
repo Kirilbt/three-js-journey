57937f0cab91d90c56b9a2c6fa004c03eec26b27
Kiril Tucker <kiril.tucker@gmail.com>
2023-03-03 18:09:08 +0100

    42 - Adding More Meshes

--- 42-first-r3f-application/src/Experience.jsx
+++ 42-first-r3f-application/src/Experience.jsx
@@ -2,9 +2,17 @@ export default function Experience() {
 
 
   return <>
+    <mesh position={[-2, 0, 0]}>
+      <sphereGeometry />
+      <meshBasicMaterial color="orange" wireframe />
+    </mesh>
     <mesh position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
       <boxGeometry />
-      <meshBasicMaterial color="mediumpurple" wireframe />
+      <meshBasicMaterial color="mediumpurple" />
+    </mesh>
+    <mesh position-y={-1} scale={10} rotation-x={- Math.PI * 0.5}>
+      <planeGeometry />
+      <meshBasicMaterial color="green" />
     </mesh>
   </>
 }
