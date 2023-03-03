ef3746e4fb939061b2d25c2e7dee7fd3d97f8fa2
Kiril Tucker <kiril.tucker@gmail.com>
2023-03-03 18:16:31 +0100

    42 - Animating

--- 42-first-r3f-application/src/Experience.jsx
+++ 42-first-r3f-application/src/Experience.jsx
@@ -1,12 +1,19 @@
-export default function Experience() {
+import { useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
 
+export default function Experience() {
+  const cubeRef = useRef()
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += 0.02
+  })
 
   return <>
     <mesh position={[-2, 0, 0]}>
       <sphereGeometry />
       <meshBasicMaterial color="orange" wireframe />
     </mesh>
-    <mesh position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
+    <mesh ref={ cubeRef } position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
       <boxGeometry />
       <meshBasicMaterial color="mediumpurple" />
     </mesh>
