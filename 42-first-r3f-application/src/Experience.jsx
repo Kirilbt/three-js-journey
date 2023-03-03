f6e7478805525117bcefb643a66dc64bfb0bd52f
Kiril Tucker <kiril.tucker@gmail.com>
2023-03-03 18:20:08 +0100

    42 - Creating Groups

--- 42-first-r3f-application/src/Experience.jsx
+++ 42-first-r3f-application/src/Experience.jsx
@@ -2,21 +2,25 @@ import { useFrame } from '@react-three/fiber'
 import { useRef } from 'react'
 
 export default function Experience() {
+  const groupRef = useRef()
   const cubeRef = useRef()
 
   useFrame((state, delta) => {
+    groupRef.current.rotation.y += 0.01
     cubeRef.current.rotation.y += 0.02
   })
 
   return <>
-    <mesh position={[-2, 0, 0]}>
-      <sphereGeometry />
-      <meshBasicMaterial color="orange" wireframe />
-    </mesh>
-    <mesh ref={ cubeRef } position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
-      <boxGeometry />
-      <meshBasicMaterial color="mediumpurple" />
-    </mesh>
+    <group ref={ groupRef }>
+      <mesh position={[-2, 0, 0]}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" wireframe />
+      </mesh>
+      <mesh ref={ cubeRef } position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
+        <boxGeometry />
+        <meshBasicMaterial color="mediumpurple" />
+      </mesh>
+    </group>
     <mesh position-y={-1} scale={10} rotation-x={- Math.PI * 0.5}>
       <planeGeometry />
       <meshBasicMaterial color="green" />
