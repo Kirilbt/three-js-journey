494c7e05bac6460b6d48baccc1b795d44270d6d6
Kiril Tucker <kiril.tucker@gmail.com>
2023-03-03 11:12:28 +0100

    41 - Loop

--- 41-first-react-application/src/App.jsx
+++ 41-first-react-application/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import Clicker from './Clicker'
 
-export default function App({ children }) {
+export default function App({ clickersCount, children }) {
   const [ hasClicker, setHasClicker ] = useState(true)
   const [ count, setCount ] = useState(0)
 
@@ -20,9 +20,14 @@ export default function App({ children }) {
 
     <button onClick={ toggleClickerClick }>{ hasClicker ? 'Clear' : 'Show' } Clicker</button>
     { hasClicker && <>
-      <Clicker increment={ increment } keyName="countA" color={ `hsl(${ Math.random() * 360 }deg, 100%, 30%)` } />
-      <Clicker increment={ increment } keyName="countB" color={ `hsl(${ Math.random() * 360 }deg, 100%, 30%)` } />
-      <Clicker increment={ increment } keyName="countC" color={ `hsl(${ Math.random() * 360 }deg, 100%, 30%)` } />
+      { [...Array(clickersCount)].map((_, index) =>
+        <Clicker
+          key={ index }
+          increment={ increment }
+          keyName={ `count${index}` }
+          color={ `hsl(${ Math.random() * 360 }deg, 100%, 30%)` }
+        />
+      ) }
     </> }
   </>
 }
--- 41-first-react-application/src/index.jsx
+++ 41-first-react-application/src/index.jsx
@@ -8,7 +8,7 @@ const toto = 'there'
 
 root.render(
   <div>
-    <App>
+    <App clickersCount = { 3 }>
       <h1>My First React App</h1>
       <h2>And a fancy subtitle</h2>
     </App>
