472d8b1518b666988bfcae148887de7930e01cf5
Kiril Tucker <kiril.tucker@gmail.com>
2023-03-03 18:00:21 +0100

    42 - Creating & Handling Meshes

--- 42-first-r3f-application/src/Experience.jsx
+++ 42-first-r3f-application/src/Experience.jsx
@@ -1,9 +1,10 @@
 export default function Experience() {
+
+
   return <>
-    <mesh>
-      <torusKnotGeometry />
-      <meshNormalMaterial />
+    <mesh position={[2, 0, 0]}  scale={1.5} rotation-y={Math.PI * 0.25}>
+      <boxGeometry />
+      <meshBasicMaterial color="mediumpurple" wireframe />
     </mesh>
   </>
-
 }
